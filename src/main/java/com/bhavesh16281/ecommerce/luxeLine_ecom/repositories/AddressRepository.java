721d81f2c130cd6d8ba5dded4f36c76d0505a7a6
Muthyalu Bhavesh <66527078+bhavesh16281@users.noreply.github.com>
2026-03-26 10:33:56 +0530

    Started Implementing API's for Address management

---
+++ src/main/java/com/bhavesh16281/ecommerce/luxeLine_ecom/repositories/AddressRepository.java
@@ -0,0 +1,7 @@
+package com.bhavesh16281.ecommerce.luxeLine_ecom.repositories;
+
+import com.bhavesh16281.ecommerce.luxeLine_ecom.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
